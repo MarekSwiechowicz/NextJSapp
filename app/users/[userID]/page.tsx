95cf98858c2f9c65a05ef3b67c2bfefa6a23a922
Marek <maswiecho@gmail.com>
2023-10-21 04:14:45 +0200

    fetching data + metadata

--- app/users/[userID]/page.tsx
+++ app/users/[userID]/page.tsx
@@ -3,12 +3,25 @@ import getUserPosts from "@/lib/getUserPosts";
 import getUser from "@/lib/getUser";
 import { Suspense } from "react";
 import UserPosts from "./components/UserPosts";
+import type { Metadata } from "next";
 
 type Params = {
   params: {
     userID: string;
   };
 };
+
+export async function generateMetadata({
+  params: { userID },
+}: Params): Promise<Metadata> {
+  const userData: Promise<User> = getUser(userID);
+  const user: User = await userData;
+
+  return {
+    title: user.name,
+    description: `This is a page of ${user.name}`,
+  };
+}
 
 export default async function UserPage({ params: { userID } }: Params) {
   const userData: Promise<User> = getUser(userID);
